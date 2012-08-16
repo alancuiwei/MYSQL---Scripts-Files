a5e51b01e79b2d92e59a948a303e1be9798632e1
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-08-16 16:02:50 +0800

    32658187

--- stock.rb
+++ stock.rb
@@ -1,33 +1,34 @@
 #encoding: utf-8
 require "open-uri"
-
-#当日股票数据
+require 'date'
 
 now_time=Time.new
-c=now_time.year.to_i
-a=now_time.month.to_i
-b=now_time.day.to_i
-a=a-1
-d=b-1
-h=open('Stocks/stock/file2.txt')                   #获取当日股票数据
+a=now_time.min.to_s
+b=(a[-1,1].to_i/2+1).to_s
+puts g='Stocks/file'+b+'.txt'
+h=open(g)                
 line_array=h.readlines
 ss=line_array.length
-
 j=0	   
 while(j<ss)                       
    begin
-   puts g=line_array[j].gsub(/\n/,'')
-   #g='600000.SS'
+   if(g=line_array[j].split('.')[1].gsub(/\n/,'').chomp=='ss')
+   g='sh'+line_array[j].split('.')[0].chomp
+   #g='ss600000'
+   else
+   g='sz'+line_array[j].split('.')[0].chomp
+  end
+  gg='Stocks/'+line_array[j].gsub(/\n/,'').chomp+'.csv'
+  puts source='http://hq.sinajs.cn/list='+g.chomp
 
-   source='http://table.finance.yahoo.com/table.csv?s='+g+'&a='+a.to_s+'&b='+d.to_s+'&c='+c.to_s+'&d='+a.to_s+'&e='+b.to_s+'&f='+c.to_s+'&ignore=.csv'
-   file='Stocks/stock/'+g+'.csv'
-   aFile = File.new(file,"w") 
+   logfile=open(gg,'a')
    open(source){|x|
      while line = x.gets                      
-         aFile.puts line     
+       logfile.puts line.split(',')[30]+','+line.split(',')[1]+','+line.split(',')[4]+','+line.split(',')[5]+','+line.split(',')[3]+','+line.split(',')[8] 
+	 #  logfile.puts line.split(',')[0].split('"')[1]+','+line_array[j].gsub(/\n/,'')+','+line.split(',')[30]+','+line.split(',')[1]+','+line.split(',')[4]+','+line.split(',')[5]+','+line.split(',')[3]+','+line.split(',')[8] 
      end 
-         aFile.close 
-    }
+   }     
+   logfile.close
    rescue
    j=j+1
    next
@@ -35,28 +36,76 @@ while(j<ss)
    j=j+1
 end
 
-#=begin
-h=open('Stocks/stock/file2.txt')            #将今日股票数据插入对应CSV文件
+
+
+=begin
+#encoding: utf-8
+require "open-uri"
+
+h=open('Stocks/file.txt')                
 line_array=h.readlines
 ss=line_array.length
+j=0	   
+while(j<ss)                       
+   begin
+   if(g=line_array[j].split('.')[1].gsub(/\n/,'').chomp=='ss')
+   g='sh'+line_array[j].split('.')[0].chomp
+   #g='ss600000'
+   else
+   g='sz'+line_array[j].split('.')[0].chomp
+  end
+  gg='Stocks/'+line_array[j].gsub(/\n/,'').chomp+'.csv'
+  puts source='http://hq.sinajs.cn/list='+g.chomp
 
+   logfile=open(gg,'a')
+   open(source){|x|
+     while line = x.gets                      
+       logfile.puts line.split(',')[30]+','+line.split(',')[1]+','+line.split(',')[4]+','+line.split(',')[5]+','+line.split(',')[3]+','+line.split(',')[8] 
+	 #  logfile.puts line.split(',')[0].split('"')[1]+','+line_array[j].gsub(/\n/,'')+','+line.split(',')[30]+','+line.split(',')[1]+','+line.split(',')[4]+','+line.split(',')[5]+','+line.split(',')[3]+','+line.split(',')[8] 
+     end 
+   }     
+   logfile.close
+   rescue
+   j=j+1
+   next
+   end
+   j=j+1
+end
+=end
+
+=begin
+#encoding: utf-8
+require "open-uri"
+g='sz184706'
+gg='Stocks/184706.sz.csv'
+source='http://hq.sinajs.cn/list='+g.chomp
+logfile=open(gg,'a')
+open(source){|x|
+  while line = x.gets                      
+  logfile.puts line.split(',')[30]+','+line.split(',')[1]+','+line.split(',')[4]+','+line.split(',')[5]+','+line.split(',')[3]+','+line.split(',')[8] 
+  #  logfile.puts line.split(',')[0].split('"')[1]+','+line_array[j].gsub(/\n/,'')+','+line.split(',')[30]+','+line.split(',')[1]+','+line.split(',')[4]+','+line.split(',')[5]+','+line.split(',')[3]+','+line.split(',')[8] 
+   end 
+  }     
+logfile.close
+=end
+#  http://download.finance.yahoo.com/d/quotes.csv?s=600000.ss&f=sd1ohgp3v     
+
+#http://hq.sinajs.cn/list=sh600000
+
+=begin
+#encoding: utf-8
+require "open-uri"
+
+h=open('Stocks/file.txt')                
+line_array=h.readlines
+ss=line_array.length
 j=0	   
 while(j<ss)                       
    puts g=line_array[j].gsub(/\n/,'')
-   #g='600000.SS'
-   gg='Stocks/stock/'+g+'.csv'
-   h=open(gg)            
-   line=h.readlines
-   s=line.length
-   ggg='Stocks/'+g+'.csv'
-   logfile=open(ggg,'a')
- #  i=s-1
- #  while(i>0)
- #  puts line[i]
-   logfile.puts line[1]
- #  i=i-1
- #  end
-   logfile.close
+   file='Stocks/'+g.chomp+'.csv'
+   aFile = File.new(file,"w") 
+    aFile.puts 'Date,Open,High,Low,Close,Volume'
+   aFile.close 
    j=j+1
 end
-#=end
+=end
