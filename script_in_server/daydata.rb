a0067a475df6e37467a3742cbdae3f0ce80be71a
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-09-12 14:31:54 +0800

    35727515

---
+++ script_in_server/daydata.rb
@@ -0,0 +1,118 @@
+#encoding:utf-8
+require 'rubygems'
+require "mysql"
+require "open-uri"
+
+
+#读取数据库
+dbh = Mysql.real_connect('localhost', 'root', '123456', 'futuretest',3306) 
+sth=dbh.prepare("select * from marketdaydata_t ") 
+sth.execute 
+file = File.new("../tongtianshun/app/assets/historydatadownload/daydata/data.txt", "w+")
+while row=sth.fetch do 
+   file.puts row 
+end 
+file.close()
+
+h=open('../tongtianshun/app/assets/historydatadownload/daydata/data.txt')                
+line=h.readlines
+ss=line.length
+j=0	  
+while(j<ss)                       
+   g=line[j].gsub(/\n/,'')
+   if(g=~/^[a-zA-Z]{1,2}\d{3,4}$/)          
+       gg='../tongtianshun/app/assets/historydatadownload/daydata/'+line[j+10].gsub(/\n/,'')+'/'+g+'.csv'
+	   if(not File.exist?(gg))
+       aFile = File.new(gg,"w") 
+       aFile.puts 'Contractid,Date,Open,High,Low,Close,Volume,Openinterest'
+       aFile.close 	   
+       end
+	   logfile=open(gg,'a')                    
+       logfile.puts g+','+line[j+1].split(' ')[0]+','+line[j+2].gsub(/\n/,'')+','+line[j+3].gsub(/\n/,'')+','+
+	                      line[j+4].gsub(/\n/,'')+','+line[j+5].gsub(/\n/,'')+','+line[j+6].gsub(/\n/,'')+','+line[j+7].gsub(/\n/,'')
+       logfile.close
+   end
+  p j=j+1
+end
+
+
+
+
+
+
+
+=begin
+i=0
+for(i<s)
+ll=l[i]+'.zip'
+def compress   
+  Zip::ZipFile.open ll, Zip::ZipFile::CREATE do |zip|   
+    add_file_to_zip('../tongtianshun/app/assets/historydatadownload/daydata', zip)   
+  end  
+end
+i=i+1
+end
+
+def add_file_to_zip(file_path, zip)   
+  if File.directory?(file_path)   
+    Dir.foreach(file_path) do |sub_file_name|   
+      add_file_to_zip("#{file_path}/#{sub_file_name}", zip) unless sub_file_name == '.' or sub_file_name == '..'  
+    end  
+  else  
+    zip.add(file_path, file_path)   
+  end  
+end
+=end
+
+
+
+=begin
+#encoding: utf-8
+require "open-uri"
+#创建所有csv文件
+h=open('a.txt')                
+line_array=h.readlines
+ss=line_array.length
+j=0	   
+while(j<ss)                       
+   puts g=line_array[j].gsub(/\n/,'')
+   file='daydata/'+g+'.csv'
+   aFile = File.new(file,"w") 
+   aFile.puts 'Contractid,Date,Open,High,Low,Close,Volume,Openinterest'
+   aFile.close 
+   j=j+1
+end
+#插入数据
+h=open('marketdaydata_t.txt')                
+line_array=h.readlines
+ss=line_array.length
+j=0	   
+while(j<ss)           
+   puts j            
+   g=line_array[j].split(',')[0]
+   gg='daydata/'+g+'.csv'
+
+   logfile=open(gg,'a')                    
+       logfile.puts line_array[j].split(',')[0]+','+line_array[j].split(',')[1]+','+line_array[j].split(',')[2]+','+line_array[j].split(',')[3]+','+
+	                line_array[j].split(',')[4]+','+line_array[j].split(',')[5]+','+line_array[j].split(',')[6]+','+line_array[j].split(',')[7]
+   logfile.close
+
+   j=j+1
+end
+=end
+
+
+
+=begin
+#C:\Documents and Settings\All Users\Application Data\MySQL\MySQL Server 5.6\data\futuretest
+#txt
+INTO OUTFILE 'a.txt'
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n';
+#csv
+SELECT * FROM mytable  
+INTO OUTFILE 'mytable.csv' 
+FIELDS TERMINATED BY ','  
+OPTIONALLY ENCLOSED BY '"'  
+LINES TERMINATED BY '\n'； 
+=end
---
+++ script_in_server/daydata.sh
@@ -0,0 +1,138 @@
+#!/bin/sh
+
+cd /ZRSoftware/Tools/
+echo 'cd /ZRSoftware/Tools/'
+/usr/local/rvm/bin/ruby  /ZRSoftware/Tools/daydata.rb
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+rm a.zip ag.zip al.zip au.zip b.zip c.zip CF.zip cu.zip ER.zip fu.zip IF.zip j.zip l.zip m.zip ME.zip OI.zip p.zip pb.zip PM.zip rb.zip RI.zip RO.zip ru.zip SR.zip TA.zip v.zip WH.zip wr.zip WS.zip WT.zip y.zip zn.zip
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/a/
+zip -m ./a.zip ./*
+mv a.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/ag/
+zip -m ./ag.zip ./*
+mv ag.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/al/
+zip -m ./al.zip ./*
+mv al.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/au/
+zip -m ./au.zip ./*
+mv au.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/b/
+zip -m ./b.zip ./*
+mv b.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/c/
+zip -m ./c.zip ./*
+mv c.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/CF/
+zip -m ./CF.zip ./*
+mv CF.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/cu/
+zip -m ./cu.zip ./*
+mv cu.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/ER/
+zip -m ./ER.zip ./*
+mv ER.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/fu/
+zip -m ./fu.zip ./*
+mv fu.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/IF/
+zip -m ./IF.zip ./*
+mv IF.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/j/
+zip -m ./j.zip ./*
+mv j.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/l/
+zip -m ./l.zip ./*
+mv l.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/m/
+zip -m ./m.zip ./*
+mv m.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/ME/
+zip -m ./ME.zip ./*
+mv ME.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/OI/
+zip -m ./OI.zip ./*
+mv OI.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/p/
+zip -m ./p.zip ./*
+mv p.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/pb/
+zip -m ./pb.zip ./*
+mv pb.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/PM/
+zip -m ./PM.zip ./*
+mv PM.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/rb/
+zip -m ./rb.zip ./*
+mv rb.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/RI/
+zip -m ./RI.zip ./*
+mv RI.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/RO/
+zip -m ./RO.zip ./*
+mv RO.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/ru/
+zip -m ./ru.zip ./*
+mv ru.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/SR/
+zip -m ./SR.zip ./*
+mv SR.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/TA/
+zip -m ./TA.zip ./*
+mv TA.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/v/
+zip -m ./v.zip ./*
+mv v.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/WH/
+zip -m ./WH.zip ./*
+mv WH.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/wr/
+zip -m ./wr.zip ./*
+mv wr.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/WS/
+zip -m ./WS.zip ./*
+mv WS.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/WT/
+zip -m ./WT.zip ./*
+mv WT.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/y/
+zip -m ./y.zip ./*
+mv y.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+cd /ZRSoftware/tongtianshun/app/assets/historydatadownload/daydata/zn/
+zip -m ./zn.zip ./*
+mv zn.zip /ZRSoftware/tongtianshun/app/assets/historydatadownload/
+
+echo 'end'
